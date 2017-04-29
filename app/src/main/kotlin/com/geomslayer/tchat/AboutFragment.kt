29a2db419c60cda4eeb0bba85839afe6d3942042
geomslayer <geomslayer@gmail.com>
2017-04-29 18:43:01 +0300

    Add animation on about screen

--- app/src/main/kotlin/com/geomslayer/tchat/AboutFragment.kt
+++ app/src/main/kotlin/com/geomslayer/tchat/AboutFragment.kt
@@ -1,12 +1,17 @@
 package com.geomslayer.tchat
 
 import android.os.Bundle
+import android.support.transition.Fade
+import android.support.transition.TransitionManager
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_about.view.*
 
 class AboutFragment : Fragment() {
+
+    lateinit var fragmentView: ViewGroup
 
     companion object {
         fun newInstance(): AboutFragment {
@@ -15,8 +20,16 @@ class AboutFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentView = inflater.inflate(R.layout.fragment_about, container, false) as ViewGroup
+        animate(true)
+        return fragmentView
+    }
+
+    private fun animate(visible: Boolean) {
+        TransitionManager.beginDelayedTransition(fragmentView, Fade().setDuration(2000))
+        fragmentView.aboutTextView.visibility = if (visible) View.VISIBLE else View.GONE
+        fragmentView.aboutTextView.postDelayed({ animate(!visible) }, 2000)
     }
 
 }
